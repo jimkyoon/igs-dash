be3bcc250c413735faf128dcc5019cb81b0d0239
Jim Yoon <jimyoon90@gmail.com>
2021-08-27 13:22:34 -0700

    made components, deleted files/folders

---
+++ src/components/button.js
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+const Button = styled.button`
+  outline: none;
+  background: black;
+  color: white;
+`;
+
+export default Button;
---
+++ src/components/textInput.js
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+
+const TextInput = styled.input`
+  outline: none;
+  border-radius: 50px;
+`;
+
+export default TextInput;
